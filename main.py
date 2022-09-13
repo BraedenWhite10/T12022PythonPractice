11402615e075ce53868314aad8ae8362b20baa69
BraedenWhite10 <johndo@mystma.org>
2022-09-13 09:41:01 -0500

    3 Functions

---
+++ main.py
@@ -0,0 +1,12 @@
+def firstAndLastName():
+    print("Braeden White")    
+
+def top5Games():
+    print("1.Just Cause 4, 2. Minecraft, 3. Fortnite, 4. Apex 5.Call of Duty") 
+
+def top3FavoriteFoods():
+    print("1.Cheeseburgers, 2. Cheese Curds, 3.Tacos")
+
+firstAndLastName()
+top5Games()
+top3FavoriteFoods()
